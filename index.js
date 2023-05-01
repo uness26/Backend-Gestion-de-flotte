6350a887d4e4ef39d34b773facc82e1f2b0ca84f
younes <yyaa096@gmail.com>
2023-05-01 23:31:59 +0200

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 require('./database/mongoose')
+const cors = require('cors')
 const userRouter = require('./routers/user')
 const vehiculeRouter = require('./routers/vehicule')
 const reclamationRouter = require('./routers/reclamation')
 const missionRouter = require('./routers/mission')
 
+
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.use(express.json())
+app.use(cors())
 app.use(userRouter)
 app.use(vehiculeRouter)
 app.use(reclamationRouter)
@@ -17,3 +20,5 @@ app.use(missionRouter)
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
+
+
